9e69c261ca3e393408fd2e211be0d043a7f1c097
Juan Sebastian Ulloa <jseb.ulloa@gmail.com>
2020-11-05 13:10:14 -0500

    audio dataset included

--- simple_step_by_step.R
+++ simple_step_by_step.R
@@ -3,11 +3,13 @@
 library(viridis)
 library(tuneR)
 library(seewave)
-
 source('graph_soundscape_fcns.R')
-setwd('/Volumes/lacie_exfat/BST_ejemplo_PPII/audio/VALLES/V6DA/')
-path_files = '/Volumes/lacie_exfat/BST_ejemplo_PPII/audio/VALLES/V6DA/'  # Folder location of acoustic dataset
-# ---------------
+
+# Set variables 
+path_files = '/Volumes/lacie_exfat/BST_ejemplo_PPII/audio/VALLES/V6DA/'  # location of audio dataset
+path_save_gs = './dataframes_gs/example_gs.csv'  # location to save the dataframe
+path_save_fig = './figures/example_gs.png'  # location to save the figure
+# -----------------
 
 flist = list.files(path_files, recursive = T, pattern = '.WAV', ignore.case = T)
 
@@ -16,16 +18,12 @@ fname = flist[1]
 fname_path = paste(path_files, fname, sep='')
 s = readWave(fname_path)
 
+# Check audio and set thrsehold for fpeaks
 oscillo(s)
-
-spec(s, wl = 512)
-
 mspec = meanspec(s, wl = 256, wn = 'hanning', norm = F, plot=T)
 peaks = fpeaks(mspec, threshold = 0.003, freq = 0, plot=T)
 
-spectro(s, wl=512, ovlp=0.5)
-
-# batch process a directory
+# Batch process a directory
 df = metadata_audio(flist, path_files, verbose = T, rec_model = 'SM')
 
 plot_sampling(df, y_axis_factor = df$sm_model, color_factor = df$sm_model, 
@@ -35,12 +33,16 @@ ggplot(df, aes(y=hour)) + geom_bar(width=0.3, alpha=0.5) + theme_minimal()
 
 ggplot(df, aes(y=day)) + geom_bar(width=0.3, alpha=0.5)
 
-gs = graphical_soundscape(df, spec_wl=256, fpeaks_th=0.003, fpeaks_f=0, verbose=T) # inicial 0.002
+# Set full path on dataframe, column path_audio
+df$fname = df$path_audio
+df$path_audio = paste(path_files,df$fname,sep='')
+
+# Compute graphical soundscape
+gs = graphical_soundscape(df, spec_wl=256, fpeaks_th=0.003, fpeaks_f=0, verbose=T) 
 plot_graphical_soundscape(gs)
 
-# save
-write.csv(gs, file='/Volumes/lacie_exfat/BST_ejemplo_PPII/graphical_soundscapes/V8DA.csv', row.names = F)
-
-png('/Volumes/lacie_exfat/BST_ejemplo_PPII/figures/V8DA.png')
+# Save dataframe and plot
+write.csv(gs, file=path_save_gs, row.names = F)
+png(path_save_fig)
 plot_graphical_soundscape(gs)
 dev.off()
