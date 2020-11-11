5b81fe9376d995e1f09ca62175ede77ebb4b4e42
Juan Sebastian Ulloa <jseb.ulloa@gmail.com>
2020-11-11 14:50:21 -0500

    update

--- batch_compute_graph_soundscape.R
+++ batch_compute_graph_soundscape.R
@@ -8,15 +8,38 @@ library(viridis)
 library(vegan)
 
 ## SET VARIABLES
-path_dataset = '/Volumes/lacie_exfat/BST_ejemplo_PPII/audio/'
-path_metadata = '/Volumes/lacie_exfat/BST_ejemplo_PPII/metadata/audio_metadata.csv'
-path_save = '/Volumes/lacie_exfat/BST_ejemplo_PPII/graphical_soundscapes/'
+path_dataset = '/Volumes/lacie_exfat/BST_ejemplo_PPII/audio/' # location of audio dataset
 sites = c("C1BO","C2BO","C3GU","C4GU","C5GU","C6CE","V1TO","V2TO","V3HU","V4HU","V5DA","V6DA")
 
-## COMPUTE GRAPH SOUNDSCAPE FOR EACH RECORDING
-# Please follow the step by step presented in read_audio_metadata to get all
-df = read.csv(path_metadata)
 
+## 1. BATCH READ AUDIO METADATA
+path_save_metadata = '/Volumes/lacie_exfat/BST_ejemplo_PPII/metadata/audio_metadata.csv' # Filename of csv that will store the metadata
+
+flist = list.files(path_dataset, recursive = T, pattern = '.WAV', ignore.case = T)
+df = metadata_audio(flist, path_dataset, verbose = T, rec_model = 'SM')
+## post-process to include factors or sites
+aux = strsplit(df$path_audio, split = '/')
+aux_df = as.data.frame(do.call(rbind, aux))
+head(aux_df)
+# set treatment, site and short file name
+df['treatment'] = aux_df$V1
+df['site'] = aux_df$V2
+df['fname_audio'] = aux_df$V3
+# Check dataframe manually
+head(df)
+tail(df)
+table(df$site)  # number of recordings per site
+# plot sampling
+plot_sampling(df, y_axis_factor = df$site, color_factor = df$site, shape_factor = df$treatment, plot_title = 'Example')
+# save dataframe to csv
+write.table(df, fpath_save, sep=',', col.names = TRUE, row.names = FALSE)
+
+
+## 2. COMPUTE GRAPH SOUNDSCAPE FOR EACH RECORDING
+path_metadata = '/Volumes/lacie_exfat/BST_ejemplo_PPII/metadata/audio_metadata.csv' 
+path_save = './dataframes_gs/'
+
+df = read.csv(path_metadata) 
 for(site in sites){
     df_site = df[df$site==site,]
     gs = graphical_soundscape(df_site, spec_wl=256, fpeaks_th=0.003, fpeaks_f=0, verbose=T) # inicial 0.002
@@ -25,9 +48,10 @@ for(site in sites){
     }
 
 
-## PLOT
-path_gs = '/Volumes/lacie_exfat/BST_ejemplo_PPII/graphical_soundscapes/'
-path_save_figure = '/Volumes/lacie_exfat/BST_ejemplo_PPII/figures/'
+## 3. PLOT
+path_gs = './dataframes_gs/'
+path_save_figure = './figures/'
+
 for(site in sites){
     gs = read.csv(paste(path_gs,site,'.csv',sep=''))
     png(paste(path_save_figure,site,'.png',sep=''))
